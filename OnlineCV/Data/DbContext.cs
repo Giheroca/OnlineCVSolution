8acd1d543e5ec9ce30f522a5611833e2195396f7
Deathunterr <s1139437@student.windesheim.nl>
2024-02-22 14:25:21 +0100

    Add project files.

---
+++ OnlineCV/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineCV.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using OnlineCV.Models;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContactRequest> ContactRequests { get; set; } 
+    }
+}
---
+++ OnlineCV/Models/ContactRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineCV.Models
+{
+	public class ContactRequest
+	{
+
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string Email { get; set; }
+		public string PhoneNumber { get; set; }
+		public string message { get; set; }
+
+	}
+}
